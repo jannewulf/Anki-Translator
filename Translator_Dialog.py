0390d766e3001464c94077718ad2816464dece5f
Janne Wulf <jannewulf@gmail.com>
2016-09-28 00:14:27 +0200

    Created Gui Class

---
+++ Translator_Dialog.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt4.QtGui import *
+
+class Translator_Dialog:
+
+    def __init__(self, vocable)
+        self.vocable = vocable
+
+    def setupUi(self):
+        
+
+    def show():
+        setupUi()
