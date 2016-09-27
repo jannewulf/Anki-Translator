e1e460c360cffbfe63eb377bb4c481ebe6b3a7ca
Janne Wulf <jannewulf@gmail.com>
2016-09-27 23:54:20 +0200

    Created Dialog Class

--- Translator.py
+++ Translator.py
@@ -1,10 +1,13 @@
 from anki.hooks import addHook
 from aqt.editor import Editor
-from aqt.utils import showInfo
+from aqt.utils import *
+import Translator_Dialog
 
 
 def getTranslation(editor):
-	showInfo("Looking for Translation")
+	#showInfo("Looking for Translation")
+	widget = Translator_Dialog()
+	widget.show()
 
 def initTranslatorGUI(editor):
 	editor._addButton("Translate", lambda ed=editor: getTranslation(ed),
---
+++ Translator_Dialog.py
@@ -0,0 +1,5 @@
+import sys
+from PyQt4.QtGui import *
+
+class Translator_Dialog:
+    
