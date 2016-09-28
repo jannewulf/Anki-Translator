4a047c4cc4bca35b581f229b4b30bd4eb4ecd8b1
Janne Wulf <jannewulf@gmail.com>
2016-09-28 02:31:41 +0200

    Changed way of button integration

--- Translator.py
+++ Translator.py
@@ -1,14 +1,19 @@
-from anki.hooks import addHook
+from anki.hooks import wrap
 from aqt.editor import Editor
 from aqt.utils import showInfo
+from TranslatorAddon.TranslatorDialog import TranslatorDialog
 
+# This functino gets executed when the button in the editor is pressed
+def getTranslation(self):
+	#showInfo("Looking for Translation")
+	dialog = TranslatorDialog(self)
+	showInfo(dialog.vocable)
 
-def getTranslation(editor):
-	showInfo("Looking for Translation")
+# Definition of the new button
+def mySetupButtons(self):
+	self._addButton("Translate", lambda ed=self: getTranslation(ed),
+				text="T", tip="Translate Word (Ctrl+T)", key="Ctrl+t")
 
-def initTranslatorGUI(editor):
-	editor._addButton("Translate", lambda ed=editor: getTranslation(ed),
-					text="T", tip="Translate Word (Ctrl+T)", key="Ctrl+t")
-
-Editor.getTranslation = getTranslation
-addHook("setupEditorButtons", initTranslatorGUI)
+# Concatenate Editor.setupButtons with mySetupButtons
+# So that a new button is inserted into the Editor
+Editor.setupButtons = wrap(Editor.setupButtons, mySetupButtons)
