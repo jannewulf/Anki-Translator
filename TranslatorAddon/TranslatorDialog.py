9a629291f20a766aeb1b44dce967de9ee40e84b3
Janne Wulf <jannewulf@gmail.com>
2016-09-28 13:47:46 +0200

    Connected function to translate button

--- TranslatorAddon/TranslatorDialog.py
+++ TranslatorAddon/TranslatorDialog.py
@@ -1,16 +1,18 @@
 from PyQt4.QtGui import *
 from PyQt4.QtCore import Qt
+from aqt.utils import showInfo
 
 class TranslatorDialog(QDialog):
 
     def __init__(self, editor):
         super(TranslatorDialog, self).__init__()
 
-        # Extract the looked up vocable
-        self.vocable = editor.note.fields[0]
+        # Extract the looked up vocable (not updated -> use lineEdit to get current value)
+        self.editorVocable = editor.note.fields[0]
 
         # set up gui
         self.setupUi()
+        
 
     def setupUi(self):
         # Set up window
@@ -29,17 +31,27 @@ class TranslatorDialog(QDialog):
         mainLayout.addWidget(self.buttonBox)
         self.setLayout(mainLayout)
 
+
     # creates all the gui elements except for the button box on the bottom
     def createGroupBox(self):
         self.formGroupBox = QGroupBox("Group Box")
         layout = QFormLayout()
-        self.lineEditVocable = QLineEdit(self.vocable)
+
+        # vocabulary line edit
+        self.lineEditVocable = QLineEdit(self.editorVocable)
         layout.addRow(QLabel("Vocable"), self.lineEditVocable)
+
+        # translate button
         self.buttonTranslate = QPushButton("Translate")
+        self.buttonTranslate.clicked.connect(self.translate)
         layout.addRow(None, self.buttonTranslate)
+
+        # translations table
         self.tableTranslations = QTableView()
         layout.addRow(QLabel("Translations"), self.tableTranslations)
+
         self.formGroupBox.setLayout(layout)
+
 
     # creates the 'Ok' and 'Cancel' buttons
     def createButtonBox(self):
@@ -47,3 +59,7 @@ class TranslatorDialog(QDialog):
             QDialogButtonBox.Cancel)
         self.buttonBox.accepted.connect(self.accept)
         self.buttonBox.rejected.connect(self.reject)
+
+
+    def translate(self):
+        showInfo(self.lineEditVocable.text())
