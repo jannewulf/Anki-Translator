3b86ab0e72fd9a6b3d370b677a19226c3a587682
Janne Wulf <jannewulf@gmail.com>
2016-09-30 19:03:14 +0200

    Added Comments

--- TranslatorAddon/Parser/Parser.py
+++ TranslatorAddon/Parser/Parser.py
@@ -6,20 +6,22 @@ from BeautifulSoup import BeautifulSoup
 class Parser(object):
     __metaclass__ = ABCMeta
 
-
+    # returns the html document of the website
     def getHTML(self, searchTerm, sourceLang, targetLang):
         return urlopen(self.createUrl(searchTerm, sourceLang, targetLang)).read()
 
-
+    # returns a BeautifulSoup element of the website
     def getSoup(self, searchTerm, sourceLang, targetLang):
         return BeautifulSoup(self.getHTML(searchTerm, sourceLang, targetLang))
 
-
+    # Abstract Method that needs to be implemented in a Parser inheritance
+    # Returns a url to the website with the translations
     @abstractmethod
     def createUrl(self, searchTerm, sourceLang, targetLang):
         pass
 
-
+    # Abstract Method that needs to be implemented in a Parser inheritance
+    # Extracts all the translations of a website
     @abstractmethod
     def getTranslation(self, searchTerm, sourceLang, targetLang):
         pass
